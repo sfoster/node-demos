3696f38c8af32a76f201a0b16afbbf7a0b2fb179
sfoster <sam@sam-i-am.com>
2012-07-11 14:17:26 +0100

    Add websocket server and the client code to make the connection. node_modules directory is ignored in this repo so we can play along and see missing module errors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http'), 
     fs = require('fs');
+    WebSocketServer = require('ws').Server;
 
 var resources = {
   html: ''
 };
+var connections = [];
 
 function init(){
 
@@ -19,6 +21,27 @@ function init(){
 
   server.listen(8888);
   console.log("HTTP server listening for http connections on port 8888");
+
+  var wss = new WebSocketServer({port: 8080});
+
+  wss.on('connection', function(ws) {
+    console.log("new ws connected client: ", ws);
+    connections.push(ws);
+
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.on('close', function() {
+      var idx = connections.indexOf(ws);
+      console.log('client hang up, ' + idx+1 + " of " + connections.length);
+      connections.splice(idx, 1);
+      console.log(connections.length + " remain");
+      ws = null;
+    });
+  });
+
+  console.log("WebSocket server listening for ws connections on port 8080");
 
 }
 
